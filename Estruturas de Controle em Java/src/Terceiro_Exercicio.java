0ee85826d11abb2fe3edd6a33d02a37d0aa35af7
NitKiri-Evergarden <peteraugustsants@gmail.com>
2025-07-17 21:59:12 -0300

    inicio do terceiro exercicio

---
+++ Estruturas de Controle em Java/src/Terceiro_Exercicio.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Terceiro_Exercicio {
+    public static void men(String{} args){
+
+       /* 3. Escreva um código que o usuário entre com um primeiro número, um segundo número maior que o primeiro
+        e escolhe entre a opção par e impar, com isso o código deve informar todos os números pares ou
+        ímpares(de acordo com a seleção inicial) no intervalo de números informados, incluindo os números informados e
+        em ordem decrescente;
+        */
+
+        Integer number1 = null;
+        Integer number2 = null;
+        Scanner leitor = new Scanner(System.in);
+        Int impar = (number1 / number2) % == 0;
+        Int par = (number1 / number2) % == 1;
+
+        System.out.println("Informe um Numero:" );
+        number1 = leitor.nextInt();
+        System.out.println("Informe um Segundo Numero que Seja Maior do que o Primeiro numero informado enteriomente: ");
+        number2 = leitor.nextInt();
+
+        System.out.println("Escolha qua")
+
+    }
+}
